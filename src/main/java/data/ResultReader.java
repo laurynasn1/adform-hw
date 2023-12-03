d51981c14fde3c1bc1dff4b288a556d983827ef6
Laurynas Notrumas <laurynas.notrumas@mif.stud.vu.lt>
2023-12-03 16:59:45 +0200

    Add result reader

---
+++ src/main/java/data/ResultReader.java
@@ -0,0 +1,50 @@
+package data;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import proto.RecordProto;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ResultReader {
+
+    private static final String OUTPUT_TOPIC = "output";
+
+    public static void main(String[] args) throws Exception {
+        var topPar = new TopicPartition(OUTPUT_TOPIC, 0);
+        var consumer = createKafkaConsumer(topPar);
+
+        var endOffset = consumer.endOffsets(List.of(topPar)).get(topPar);
+        while (consumer.position(topPar) < endOffset) {
+            var recs = consumer.poll(Duration.ofMillis(100));
+            for (var rec : recs) {
+                RecordProto.RecordInfo result = RecordProto.RecordInfo.parseFrom(rec.value());
+
+                System.out.println("User agent: " + result.getUserAgent());
+                System.out.println("Record type: " + result.getType());
+                System.out.println("Time interval: " + result.getTimeInterval());
+                System.out.println("Record count: " + result.getRecordCount());
+                System.out.println("Average time between subsequent events: " + result.getAverageBetweenRecords());
+                System.out.println();
+            }
+        }
+    }
+
+    private static KafkaConsumer<String, byte[]> createKafkaConsumer(TopicPartition topPar) {
+        var props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, byte[]> consumer = new KafkaConsumer<>(props);
+        consumer.assign(List.of(topPar));
+        return consumer;
+    }
+}
